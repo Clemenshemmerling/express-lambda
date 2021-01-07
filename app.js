609922ebe13433f706f3d0daadfa95c07694ab5d
Clemens Hemmerling <clemens@getkor.io>
2021-01-06 19:27:57 -0600

    add body parse and express

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
 const serverlessExpressMiddleware = require('@vendia/serverless-express/middleware')
 const bodyParser = require('body-parser')
+const express = require('express')
 const app = express()
 
 app.use(bodyParser.json())
