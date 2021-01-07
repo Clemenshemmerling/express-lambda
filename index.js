9d4189ef5a2717675baadce212fca8b4368010ee
Clemens Hemmerling <clemens@getkor.io>
2021-01-07 15:27:08 -0600

    fix intregration method for api gateway

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./app');
-const server = serverlessExpress.createServer(app);
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-exports.handler = (event, context) =>  { 
-  serverlessExpress.proxy(server, event, context);
-}; 
+app.get('/', (req, res) => {
+  res.send({ application: 'sample-app', version: '1' });
+});
+
+app.post('/', (req, res) => {
+  res.send({ ...req.body });
+});
+
+module.exports.handler = serverless(app);
